a8f9325c74d2bad170e0e47825f92e8dfc28e761
mateusvvv <mat3usvvv619@gmail.com>
2025-11-02 19:58:10 -0300

    New Portfólio

---
+++ script.js
@@ -0,0 +1,13 @@
+function enviarWhats(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '81989849687';
+    const text = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(text);
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    console.log(url);
+    window.open(url, '_blank');
+}
